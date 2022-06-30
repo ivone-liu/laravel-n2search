82d0c25f80fa89d670c6f20da54ac13d294ac0f6
Ivone <i@ivone.me>
2022-06-30 11:29:59 +0800

    * fixed queue bug

--- src/Core/Load.php
+++ src/Core/Load.php
@@ -2,7 +2,6 @@
 
 namespace N2Search\Core;
 
-use Illuminate\Database\Eloquent\Builder;
 use N2Search\Jobs\ImportJob;
 use N2Search\N2Search;
 use Overtrue\Pinyin\Pinyin;
@@ -20,9 +19,11 @@ class Load
     protected $db;
     protected $columns;
     protected $n2_config;
+    protected $db_class;
 
-    public function __construct(Builder $db_model, array $columns, N2Search $n2) {
-        $this->db = $db_model;
+    public function __construct($db_class, array $columns, N2Search $n2) {
+        $this->db_class = $db_class;
+        $this->db = new $this->db_class;
         $this->n2 = $n2;
         $this->columns = $columns;
         $this->n2_config = $n2->getN2Config();
@@ -42,7 +43,7 @@ class Load
      */
     public function addOne(int $id, $need_pinyin = 0, $need_queue = 0) {
         if ($need_queue == 1) {
-            ImportJob::dispatch($this->db, $this->columns, $id, $need_pinyin, 0)->onQueue("n2_build");
+            ImportJob::dispatch($this->db_class, $this->columns, $id, $need_pinyin, 0)->onQueue("n2_build");
             return;
         }
         $log = $this->db->where(['id'=>$id])->first()->toArray();
@@ -86,7 +87,7 @@ class Load
             $log = $log->toArray();
 
             if ($need_queue == 1) {
-                ImportJob::dispatch($this->db, $this->columns, 0, $need_pinyin, 1, $log)->onQueue("n2_build");
+                ImportJob::dispatch($this->db_class, $this->columns, 0, $need_pinyin, 1, $log)->onQueue("n2_build");
                 continue;
             }
 
